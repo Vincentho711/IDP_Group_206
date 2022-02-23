9a03ce37c75dd4cd00351937d6edb207fc971d33
Junhyuck Kim <jhyuckkim@gmail.com>
2022-02-23 11:02:10 +0000

    Line following

--- Line following/src/main.cpp
+++ Line following/src/main.cpp
@@ -7,14 +7,20 @@
 #define leftIR A0
 #define MiddleIR A1
 #define RightIR A2
-#define IRthresh 400
-#define IRmargin 100
+#define IRthresh 250
+#define IRmargin 50
+
+#define SpeedStraight 150
+#define RotationSpeedLow 80
+#define RotationSpeedHigh 100
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 
-
+bool digLeft = false;
+bool digMiddle = true;
+bool digRight = false;
 
 
 
@@ -34,10 +40,6 @@ void loop() {
   int anaLeft = analogRead(leftIR);
   int anaMiddle = analogRead(MiddleIR);
   int anaRight = analogRead(RightIR);
-
-  bool digLeft;
-  bool digMiddle;
-  bool digRight;
 
   if (anaLeft >= (IRthresh + IRmargin)) {
     digLeft = true;
@@ -60,13 +62,42 @@ void loop() {
     digRight = false;
   }
 
-Serial.print("\nleft");
-Serial.print(anaLeft,digLeft);
-Serial.print("\nmiddle");
-Serial.print(anaMiddle,digMiddle);
-Serial.print("\nright");
-Serial.print(anaRight,digRight);
+  // Serial.println("left:");
+  // Serial.println(anaLeft);
+  // Serial.println(digLeft);
 
+  // Serial.println("middle:");
+  // Serial.println(anaMiddle);
+  // Serial.println(digMiddle);
 
+  // Serial.println("right:");
+  // Serial.println(anaRight);
+  // Serial.println(digRight);
 
+  Serial.print(digLeft);
+  Serial.print(digMiddle);
+  Serial.print(digRight);
+  Serial.print("\n");
+
+  delay(1000);
+  // if (digLeft==0 && digMiddle==1 && digRight==0) {
+  //   leftMotor->setSpeed(SpeedStraight);
+  //   rightMotor->setSpeed(SpeedStraight);  
+  // }
+  // else if (digLeft==1 && digRight==0) {
+  //   leftMotor->setSpeed(RotationSpeedLow);
+  //   rightMotor->setSpeed(RotationSpeedHigh);
+  // }
+
+  // else if (digLeft==0 && digRight==1) {
+  //   leftMotor->setSpeed(RotationSpeedLow);
+  //   rightMotor->setSpeed(RotationSpeedHigh);
+  // }
+
+  // else{
+  //   leftMotor->run(RELEASE);
+  //   leftMotor->setSpeed(0);
+  //   rightMotor->run(RELEASE);
+  //   rightMotor->setSpeed(0);
+  // }
 }
